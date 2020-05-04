44542231a2016e7984f030e13d10dc5b10c9500e
rcavallini <renancavallini@gmail.com>
2020-05-03 21:04:05 -0300

    Added Exercicios.py e Stage 2/4 - Exibindo camelos com quotes

---
+++ Exercicios.py
@@ -0,0 +1,15 @@
+print(type("int"))
+print(type(394))
+print(type(2.71))
+
+
+
+word1 = "King"
+word2 = "Cross"
+# add the correct string instead of the dots
+print(word1 + "\'s " + word2)
+
+print("Did that stop the old Grinch?")
+print("No! The Grinch simply said,")
+print("\"If I can't find a reindeer,")
+print("I'll make one instead!\"")
--- Zookeeper.py
+++ Zookeeper.py
@@ -4,3 +4,22 @@ print("Start looking after animals...")
 print("Deer looks fine.")
 print("Bat looks happy.")
 print("Lion looks healthy.")
+print("""Switching on camera from habitat with camels...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \\
+     |     \    _.-'             \\
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;     
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Yey, our little camel is sunbathing!""")
