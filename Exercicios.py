0398482aabdc0d4d2591d27f5ef9c49dbf5b4674
rcavallini <renancavallini@gmail.com>
2020-05-04 22:19:19 -0300

    Desenvolvido Stage 3/4

--- Exercicios.py
+++ Exercicios.py
@@ -1,15 +1,4 @@
-print(type("int"))
-print(type(394))
-print(type(2.71))
-
-
-
-word1 = "King"
-word2 = "Cross"
-# add the correct string instead of the dots
-print(word1 + "\'s " + word2)
-
-print("Did that stop the old Grinch?")
-print("No! The Grinch simply said,")
-print("\"If I can't find a reindeer,")
-print("I'll make one instead!\"")
+data = input()
+print('222222222222')
+print('Digita ai ' + data)
+print('----------')
